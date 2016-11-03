b87e7f309daff00279b7875b139255436d2b3290
Khairunisah ananda <khairunisah.ananda@yahoo.com>
2016-11-03 21:15:49 +0700

    Add files via upload

---
+++ timer_Overflow_untuk_LED_blinking.ino
@@ -0,0 +1,30 @@
+boolean dataLed = 0;
+unsigned int overflowCount = 0;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(12, OUTPUT);
+  TCCR1A = 0;
+  TCCR1B = 0;
+  TCNT1 = 34286;
+  TCCR1B |= (1<< CS12);
+  TIMSK1 |= (1<< TOIE1);
+  sei();
+}
+ISR(TIMER1_OVF_vect) {
+  TCNT1 = 34286;
+  if(dataLed == 0) {
+    dataLed = 1;
+  }
+  else {
+    dataLed = 0;
+  }
+  digitalWrite(12, dataLed);
+  overflowCount++;
+}
+
+void loop() {
+  Serial.print("Overflow count = ");
+  Serial.println(overflowCount, DEC);
+  delay(100);
+}
